40a5b5fccf8d6c668b80ed54e2789c7a046f3c65
Marcel Zwiers <m.zwiers@donders.ru.nl>
2025-12-22 13:26:55 +0100

    Clean up the huge global variable after the GUI is destroyed

--- sepia.m
+++ sepia.m
@@ -82,6 +82,7 @@ end
 % create GUI figure
 h.fig=figure('Units','pixels','position',[posLeft posBottom guiSizeHori guiSizeVert],...
     'MenuBar','None','Toolbar','None','Name',['SEPIA GUI (' SEPIA_version ')'],'NumberTitle','off');
+h.fig.CloseRequestFcn = @onClose;
 
 %% construct panels for each tab 
 % create Tabs for GUI
@@ -700,4 +701,10 @@ try
 catch
     warndlg('Missing ANTs library. All functions in Analysis Tab cannot be used.')
 end
+end
+
+function onClose(~,~)
+    global h
+    delete(h.fig)
+    clear global h
 end
